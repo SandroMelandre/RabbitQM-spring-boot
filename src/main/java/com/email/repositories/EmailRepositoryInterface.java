b27f54eb3bba0597d557b12da7a58b4f05bc04f1
sandrorezende <sandromelandre@gmail.com>
2022-07-27 14:32:21 -0300

    created controllers, repository, service

--- src/main/java/com/email/repositories/EmailRepositoryInterface.java
+++ src/main/java/com/email/repositories/EmailRepositoryInterface.java
@@ -2,7 +2,6 @@ package com.email.repositories;
 
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.email.models.EmailModel;
